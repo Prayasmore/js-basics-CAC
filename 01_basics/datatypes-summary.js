905c5bb6f118c52d5751ab78b0ddda4f00b9b5e6
Prayasmore <43825176+Prayasmore@users.noreply.github.com>
2023-11-29 03:47:57 +0000

    Memory types

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -13,7 +13,7 @@ const anotherId = Symbol("123");
 // console.log(id === anotherId);
 
 const bigNumber = 123455678565445564n;
-console.log(bigNumber);
+// console.log(bigNumber);
 
 // Reference (Non-primitive)
 // Array, Objects, Functions
@@ -29,4 +29,37 @@ const myFunction = function () {
     // console.log("Hello World");
 }
 
-console.log(typeof heroes);
+// console.log(typeof heroes);
+
+
+// **************************** Memory **************************************
+
+// Types of Memories: Stack and Heap
+// Stack memory is used in Primitive datatypes
+// Heap memory is used in Non-primitive datatypes
+
+
+let myName = "Prayas";
+
+let anotherName = myName;
+
+anotherName = "Birsu";
+
+console.log(myName);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+};
+
+let userTwo = userOne;
+
+userTwo.email = "prayas@google.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+// Stack gives the copy of the primitive datatypes (copy of the original value)
+// Heap gives the reference of the non-primitive datatypes (ref of the original value)
