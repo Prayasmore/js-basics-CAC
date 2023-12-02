a7d9ed8f624405f736aa64d2a4e73df4398b798e
Prayasmore <43825176+Prayasmore@users.noreply.github.com>
2023-12-02 15:14:31 +0000

    while and do-while loop complete

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,26 @@
+// while 
+
+// let index = 0;
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2;
+// }
+
+
+// let myArray = ["Batman", "Superman", "Flash", "Joker"];
+// let ind = 0;
+
+// while (ind < myArray.length) {
+//     console.log(`Value is ${myArray[ind]}`);
+//     ind = ind + 1;
+// }
+
+// do-while
+
+// let score = 1;
+let score = 11;
+
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
