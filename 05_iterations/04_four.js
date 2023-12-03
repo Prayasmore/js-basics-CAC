fdf4dbf8815b5958c17be5623fef4b4a0d730304
Prayasmore <43825176+Prayasmore@users.noreply.github.com>
2023-12-03 07:46:10 +0000

    for-in loop and map datatype complete

---
+++ 05_iterations/04_four.js
@@ -0,0 +1,33 @@
+// for-in
+
+const myObject = {
+    JS: "JavaScript",
+    CPP: "C++",
+    ROR: "Ruby on Rails",
+    PHP: "Hypertext Preprocessor",
+    PY: "Python" 
+};
+
+// for (const key in myObject) {
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+const programming = ["js", "rb", "py", "java", "cpp"];
+
+// for (const key in programming) {
+//     // console.log(key); 
+//     console.log(programming[key]);
+// }
+
+// const map = new Map();
+// map.set("IN", "India");
+// map.set("US", "United States of America");
+// map.set("FR", "France");
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
+// ** Map are not iterable **
+
+
